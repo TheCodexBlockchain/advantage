e17f6ecffbc6b5012f1fa48195743eec7ecd43e6
Anders <5411664+PAB84@users.noreply.github.com>
2023-03-12 11:47:40 +0100

    Remove "g++-mingw-w64-x86-64-posix" from required dependencies This package is missing in Ubuntu 18.04, and might cause the script to fail. Installing "g++-mingw-w64-x86-64" is enough.

--- build_windows.sh
+++ build_windows.sh
@@ -8,7 +8,7 @@ fi
 
 # Upgrade the system and install required dependencies
 	sudo apt update
-	sudo apt install git zip unzip build-essential libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 curl g++-mingw-w64-x86-64 g++-mingw-w64-x86-64-posix libqt5svg5-dev -y
+	sudo apt install git zip unzip build-essential libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 curl g++-mingw-w64-x86-64 libqt5svg5-dev -y
 	echo "1" | sudo update-alternatives --config x86_64-w64-mingw32-g++
 
 # Disable WSL support for Win32 applications.
