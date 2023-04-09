0a7a81b9832c54b3e4b0a7c447fff347e0d400c6
Anders <5411664+PAB84@users.noreply.github.com>
2023-04-09 14:42:10 +0200

    Remove hardcoded values, should be set automatically

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,11 +43,10 @@ const std::string CLIENT_NAME("itcoin blockchain");
 #include "obj/build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
-#define GIT_ARCHIVE 1
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "36fa54123791c3d87044247dda2d041db48aae22"
-#define GIT_COMMIT_DATE "Sat, 25 Mar 2023 16:17:14 -0600"
+#define GIT_COMMIT_ID "$Format:%H$"
+#define GIT_COMMIT_DATE "$Format:%cD$"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
